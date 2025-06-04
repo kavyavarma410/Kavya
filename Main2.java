3fc6203773609f1ea403fba8b2b4b8236ac262fd
kavyavarma410 <kavyavarmacng@gmail.com>
2025-06-04 10:52:19 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main2
+{
+public static void main(String[] args)
+{
+String name="hello";
+System.out.println("String name="+name);
+String newString=new String("hello");
+System.out.println("String new String="+newString);
+}
+}
+
